8521a6b8ce975733d73dc4db1d76f5e055106d64
akostrik <internet124816@gmail.com>
2023-04-23 05:13:39 +0200

    ascii art

--- ascii_art.py
+++ ascii_art.py
@@ -7,8 +7,8 @@ for i in range(h):
     for j in range(len(t)):
         n = 26
         if ord('a') <= ord(t[j]) and ord(t[j]) <= ord('z'):
-            n = ord(t[j])-ord('a')
+            n = ord(t[j]) - ord('a')
         if ord('A') <= ord(t[j]) and ord(t[j]) <= ord('Z'):
-            n = ord(t[j])-ord('A')
+            n = ord(t[j]) - ord('A')
         print(row[l*n:l*(n+1)], end="")
     print()
