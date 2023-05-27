4bee1256227fed303b3dfc38a27d537ba9c39726
an <internet124816@gmail.com>
2023-05-27 17:57:04 +0200

    Create power_of_Thor.c

---
+++ power_of_Thor.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    // the position of the light of power
+    int light_x;
+    int light_y;
+    // Thor's starting position
+    int initial_tx;
+    int initial_ty;
+
+    scanf("%d%d%d%d", &light_x, &light_y, &initial_tx, &initial_ty);
+
+    while (1) {
+        int remaining_turns;
+        scanf("%d", &remaining_turns);
+
+        if      (initial_tx <  light_x && initial_ty > light_y )
+        {
+            printf("NE\n");
+            initial_tx++;
+            initial_ty--;
+        }
+        else if (initial_tx <  light_x && initial_ty < light_y)
+        {
+            printf("SE\n");
+            initial_tx++;
+            initial_ty++;
+        }
+        else if (initial_tx >  light_x && initial_ty < light_y)
+        {
+            printf("SW\n");
+            initial_tx--;
+            initial_ty++;
+        }
+        else if (initial_tx >  light_x && initial_ty > light_y)
+        {
+            printf("NW\n");
+            initial_tx--;
+            initial_ty--;
+        }
+        else if (initial_ty >  light_y)
+        {
+            printf("N\n");
+            initial_ty--;
+        }
+        else if (initial_tx <  light_x )
+        {
+            printf("E\n");
+            initial_tx++;
+        }
+        else if (initial_ty <  light_y)
+        {
+            printf("S\n");
+            initial_ty++;
+        }
+        else if (initial_tx >  light_x)
+        {
+            printf("W\n");
+            initial_tx--;
+        }
+    }
+
+    return 0;
+}
