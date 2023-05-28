a3325618ba64c4dbf3358f774628cbdc9791da6e
an <internet124816@gmail.com>
2023-05-28 04:56:33 +0200

    Update power_of_Thor.c

--- power_of_Thor.c
+++ power_of_Thor.c
@@ -3,13 +3,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- * ---
- * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
- **/
-
 int main()
 {
     // the position of the light of power
